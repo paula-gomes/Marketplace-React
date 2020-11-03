4cb2e0517e92af5fa69849dac54f246e66542eac
Paula Gomes <gomes.pcristina@gmail.com>
2020-11-03 12:03:46 -0300

    product description

--- src/Components/Product/ViewProduct.js
+++ src/Components/Product/ViewProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link , Switch , Route, useParams } from 'react-router-dom';
+import { Link, Switch, Route, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -7,40 +7,37 @@ import Card from 'react-bootstrap/Card';
 import './Product.css';
 
 //this must be in another page
-function ViewProduct (props) {
+function ViewProduct(props) {
 
-  
+
   const [product, setProduct] = React.useState([]);
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(id);
   React.useEffect(() => {
-		fetch(`https://resilia-marketplace.herokuapp.com/products/${id}`)
-			.then(res => res.json())
-			.then(json => setProduct(json))
-	}, [id])
+    fetch(`https://resilia-marketplace.herokuapp.com/products/${id}`)
+      .then(res => res.json())
+      .then(json => setProduct(json))
+  }, [id])
 
-  
-    return (
-        <>
-          {
-            product.map(item=>(
-              <Card key ={item.id}>
-                <Card.Title>{item.name}
-                </Card.Title>
-              </Card>
-            ))
-            }
-          
-        </>
-    )
+
+  return (
+    <>
+      {
+        product.map(item => (
+          <Card key={item.id}>
+            <Card.Title>{item.name}</Card.Title>
+            <div className="productInfo">
+              <Card.Img className="cardImg" variant="top" src={item.img} />
+              <Card.Text>{item.description}</Card.Text>              
+            </div>
+            <Button variant="primary" style={{ width: "100px"}}> Finalizar compra</Button> 
+          </Card>
+
+        ))
+      }
+
+    </>
+  )
 }
 
-
 export default ViewProduct;
-
-/*
-  <Switch>          
-          <Route path={`/products/${id}`} />
-
-          </Switch>
-*/
